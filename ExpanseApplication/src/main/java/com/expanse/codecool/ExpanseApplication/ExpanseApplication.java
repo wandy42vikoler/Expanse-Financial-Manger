1a61b113601a43d75aec83d2fc25035b69e7fb19
Ziya Oezcifci <ziyoss68@gmail.com>
2022-09-05 14:47:56 +0200

    Started Project by String Initialzr2 2

---
+++ ExpanseApplication/src/main/java/com/expanse/codecool/ExpanseApplication/ExpanseApplication.java
@@ -0,0 +1,13 @@
+package com.expanse.codecool.ExpanseApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpanseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpanseApplication.class, args);
+	}
+
+}
