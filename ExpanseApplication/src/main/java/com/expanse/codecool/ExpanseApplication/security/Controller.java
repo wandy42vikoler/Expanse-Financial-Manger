c691d8af87befa6427b10d92cc009b4bc851e187
Ziya Oezcifci <ziyoss68@gmail.com>
2022-09-20 11:27:06 +0200

    Updated Controller

--- ExpanseApplication/src/main/java/com/expanse/codecool/ExpanseApplication/security/Controller.java
+++ ExpanseApplication/src/main/java/com/expanse/codecool/ExpanseApplication/security/Controller.java
@@ -11,8 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.stereotype.Controller
+@RestController
 public class Controller {
     private final SecurityUserDetailsService userDetailsManager;
     private final PasswordEncoder passwordEncoder;
